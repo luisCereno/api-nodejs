f04b07408bfb9e9abf7506cafee79df6eb04da46
Lucho <luis.cereno.t@gmail.com>
2019-12-22 17:45:32 -0300

    segundo commit

--- src/addNaves.js
+++ src/addNaves.js
@@ -9,8 +9,23 @@ const client = redis.createClient(6379, '127.0.0.1');
 
 app.post('/', async (req, res) => {
     const result = await getNaves();
-    result.forEach(element => {
-        client.set(element.name, JSON.stringify(element), redis.print);
+    // result.map(result => console.log('aaaaaaaaaaaaaa'+ result[0].name))
+    for (let i = 0; i < result.length; i++) {
+        const element = result[i];
+        for (let j = 0; j < array.length; j++) {
+            console.log('aer', element[j].name );
+            
+        }
+    }
+    
+    // result.forEach(element => {
+    //     console.log("respuestsa del post :", element)
+    //     client.set(element.name, JSON.stringify(element), redis.print);
+    // });
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Se agregaron las naves correctamente'
     });
 });
 
--- src/getNaves.js
+++ src/getNaves.js
@@ -4,10 +4,24 @@ const redis = require('redis');
 //Redis
 const client = redis.createClient(6379, '127.0.0.1');
 
-const getNaves = async function (req, res) {
-    const respuesta = await axios.get('https://swapi.co/api/starships/');
-    const { results } = respuesta.data;
-    return results;
+var naves = [];
+
+const axiosNaves = async (url) => {
+    const defaultUrl = 'https://swapi.co/api/starships/?page=1';
+    const respuesta = await axios.get(url || defaultUrl);
+    
+    if (respuesta.data.next) {
+        await axiosNaves(respuesta.data.next);
+    } 
+
+    naves.push(respuesta.data.results);
+    
+    return naves;
+}
+
+const getNaves = async function () {
+    const respuesta = await axiosNaves();
+    return respuesta;
 }
 
 module.exports = getNaves;
